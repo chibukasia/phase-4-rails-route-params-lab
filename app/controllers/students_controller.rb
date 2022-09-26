1e9dc7eb265f5709caaf0e7ff987e5248c0118bb
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-09-26 16:07:26 +0300

    commit tree c5ff0653b3d22dd0e427067a04d4385400214f08

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.find_by(first_name: params[:name]) || Student.find_by(last_name: params[:name])
     render json: students
   end
 
+  def show 
+    student = Student.find(params[:id])
+    render json: student
+    # byebug
+  end
 end
